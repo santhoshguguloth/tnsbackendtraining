3d962828c8bfff75213f1cd062ba6075c6f9a9a7
santhosh <santhoshgugloth6@gmail.com>
2025-08-04 21:29:16 +0530

    tnsjavatraining

---
+++ Santhosh.java
@@ -0,0 +1,13 @@
+package santhosh.example;
+
+public class Santhosh {
+    public void santhosh() {
+        System.out.println("Welcome to Eclipse IDE, Santhosh!");
+    }
+
+    public static void main(String[] args) {
+        Santhosh obj = new Santhosh();
+        obj.santhosh();
+    }
+}
+
